373156982c9726dea4e51d4934a5d366224bf9eb
minhdanh <minhdanh@users.noreply.github.com>
2021-11-05 20:01:39 +0700

    Adjust color according to the value of the number

--- scripts/ha-aqi.sh
+++ scripts/ha-aqi.sh
@@ -2,9 +2,28 @@
 
 source $HOME/.config/tmux-ha-aqi
 
-aqi=$(curl -s -k -X GET \
+raw_aqi=$(curl -s -k -X GET \
   -H "Authorization: Bearer $HA_TOKEN" \
   -H "Content-Type: application/json" \
   $HA_ENDPOINT/api/states/sensor.u_s_air_quality_index | grep -oP '"state": "(\K[^"]*)')
 
-printf "AQI: %s#[fg=default]" "$aqi"
+aqi="#[fg=green]$raw_aqi"
+if [[ -z "$raw_aqi" ]]; then
+  aqi="#[fg=default]N/A"
+fi
+if [[ "$raw_aqi" -gt "50" ]]; then
+  aqi="#[fg=yellow]$raw_aqi"
+fi
+if [[ "$raw_aqi" -gt "100" ]]; then
+  aqi="#[fg=orange]$raw_aqi"
+fi
+if [[ "$raw_aqi" -gt "150" ]]; then
+  aqi="#[fg=red]$raw_aqi"
+fi
+if [[ "$raw_aqi" -gt "200" ]]; then
+  aqi="#[fg=purple]$raw_aqi"
+fi
+if [[ "$raw_aqi" -gt "300" ]]; then
+  aqi="#[fg=brown]$raw_aqi"
+fi
+printf "#[fg=blue]AQI: %s#[fg=default]" "$aqi"
